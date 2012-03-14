569b63819e791f9cd6b80f67a6e872e23fb31e18
Arjun Prakash <archieoi@gmail.com>
2012-03-13 18:20:04 -0700

    gyantrobot 001

---
+++ js/Input.js
@@ -0,0 +1,88 @@
+// Shooter by Arjun Prakash
+// 01.29.12
+// input.js
+
+// ## Movement Functions
+
+var mouseX;
+
+function onDocumentMouseDown( event ) {
+
+	event.preventDefault();
+
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+	document.addEventListener( 'mouseout', onDocumentMouseOut, false );
+
+	mouseXOnMouseDown = event.clientX - windowHalfX;
+	targetRotationOnMouseDown = targetRotation;
+}
+
+function onDocumentMouseMove( event ) {
+
+	mouseX = event.clientX - windowHalfX;
+
+	targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
+}
+
+function onDocumentMouseUp( event ) {
+
+	document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+	document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+}
+
+function onDocumentMouseOut( event ) {
+
+	document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+	document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+}
+
+function onDocumentTouchStart( event ) {
+
+	if ( event.touches.length == 1 ) {
+
+		event.preventDefault();
+
+		mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
+		targetRotationOnMouseDown = targetRotation;
+
+	}
+}
+
+function onDocumentTouchMove( event ) {
+
+	if ( event.touches.length == 1 ) {
+
+		event.preventDefault();
+
+		mouseX = event.touches[ 0 ].pageX - windowHalfX;
+		targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
+
+	}
+}
+
+
+function onKeyDown( event ) {
+	switch( event.keyCode ) {
+
+		case 65: /*a*/ controlsRobotHead.turnLeft = true; break;
+		case 68: /*d*/ controlsRobotHead.turnRight = true; break;
+		case 13: /*Enter*/ controlsRobotHead.fire = true; break;
+
+	}
+
+
+}
+
+function onKeyUp( event ) {
+	switch( event.keyCode ) {
+
+		case 65: /*a*/ controlsRobotHead.turnLeft = false; break;
+		case 68: /*d*/ controlsRobotHead.turnRight = false; break;
+		case 13: /*Enter*/ controlsRobotHead.fire = false; break;
+
+	}
+
+}
